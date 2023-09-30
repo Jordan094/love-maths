40fe5b5843e3dec7214da928b8595470cf65ba2b
Jordan094 <>
2023-09-30 15:35:32 +0000

    Functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
-function runGame() {
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");   
+    
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the users answer has been processed.
+ */
+function runGame() {
+    //creates two random number between 1 - 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
