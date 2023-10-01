b99c4c06ae6a76eeb257311b3f689910131f8ec5
Jordan094 <>
2023-10-01 15:51:40 +0000

    Added division challenege section

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,9 @@ function runGame(gameType) {
     }
         else if (gameType === "subtract") {
             displaySubtractionQuestion(num1, num2);
-
+    }
+        else if (gameType === "division") {
+            displayDivisionQuestion(num1, num2);
     } else {
             alert(`Unknown game type: ${gameType}`);
             throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,7 +90,11 @@ function calculateCorrectAnswer() {
     }
     else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    }
+    else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }  
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -134,4 +140,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+}
 
